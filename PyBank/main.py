c30f94135b72d7d000f8f0a748f0e835a48ef0b0
spennington1394 <69980074+spennington1394@users.noreply.github.com>
2020-10-08 12:50:42 -0400

    Setting up path to begin HW
    Direct path to resources file and two csv  files.  Printed header to confirm that files are in main.py files accurately.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,13 @@
 import os 
 import csv
 
-budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+budget_csv = os.path.join("/Users/kscomputer/Desktop/DataRepository/Python-Challenge/PyBank/Resources/budget_data.csv")
 
 #open and read csv
 with open(budget_csv) as csv_file:
     csv_reader= csv.reader(csv_file, delimiter=',')
 
     csv_header = next(csv_file)
+    print(csv_header)
     
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,11 @@
 import os 
 import csv
 
-election_csv = os.path.join("..", "Resources", "election_data.csv")
+election_csv = os.path.join("/Users/kscomputer/Desktop/DataRepository/Python-Challenge/PyPoll/Resources/election_data.csv" )
 
 #open and read csv
 with open(election_csv) as csv_file:
     csv_reader= csv.reader(csv_file, delimiter=',')
 
     csv_header = next(csv_file)
+    print(csv_header)
