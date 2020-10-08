91997badc0efc32ba94f8218ce9e42841e2664dc
spennington1394 <69980074+spennington1394@users.noreply.github.com>
2020-10-08 13:20:30 -0400

    Small outline and print out analysis
    coded print out analysis and added some points for calculating greatest increase, average, and decrease.

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,5 +9,23 @@ with open(budget_csv) as csv_file:
 
     csv_header = next(csv_file)
     print(csv_header)
+
+#state variables that need to be set to 0. 
+
+#calculate greatest increase
+
+#calculate greatest lose
+
+#calculate average
+
+
+#print out results
+print(f'Financial Analysis')
+print('-----------------------')
+print('Total Months:' {total_months})
+print(f'Total Revenue $', {total_profit_loses})
+print(f'Average Revenue $', {average})
+print(f'Greatest Increase in Profits'{},{})
+print(f'Greatest Decrease in Profits' {},{})
     
         
