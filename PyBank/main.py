0e6b0f6a258abeb27d9d97b666f282f8f3b1d3ae
spennington1394 <69980074+spennington1394@users.noreply.github.com>
2020-10-08 16:27:43 -0400

    Update main.py
    removed some items and placed in a folder.  Was getting too far ahead of myself and ultimately confused. now have two lists and the length of one to give me total months

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,31 +10,13 @@ with open(budget_csv) as csv_file:
     csv_header = next(csv_file)
     print(csv_header)
 
-#state variables and variables that need to be set to 0. 
-months = []
-profit_loss_change = []
-
-total_profit_loses = 0
-total_months= 0
-
-#establish for loop
+    month = []
+    total_revenue = []
 
 
+    for row in csv_reader:
+        month.append(row[0])
+        total_revenue.append(row[1])
 
-
-#calculate greatest increase
-total_months = len(months)
-
-#calculate greatest lose
-
-#calculate average
-
-
-#print out results
-print(f'Financial Analysis')
-print('-----------------------')
-print('Total Months:' {total_months})
-print(f'Total Revenue $', {total_profit_loses})
-print(f'Average Revenue $', {average})
-print(f'Greatest Increase in Profits'{},{})
-print(f'Greatest Decrease in Profits' {},{})
+    total_month = (len(month))
+    print(total_month)
